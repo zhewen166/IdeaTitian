2cb2944f89ac0ba5a07e7141c8762d041c141c05
1013376495@qq.com <shuangzheng166>
2016-04-08 09:57:01 +0800

    create project

---
+++ src/main/scala/com/cn/spark/App.scala
@@ -0,0 +1,27 @@
+package com.cn.spark
+
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+/**
+ * Hello world!
+ *
+ */
+object App  {
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("word-count")
+    val sc = new SparkContext(conf)
+
+    //List("lib/spark-assembly_2.10-0.9.0-incubating-hadoop1.0.4.jar")
+    val logFile = sc.textFile("/home/cz/spark/learning-spark-example/README.md") // Should be some file on your system
+
+    val counts = logFile.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).collect().foreach(println)
+    println("finished-------")
+
+  }
+}
